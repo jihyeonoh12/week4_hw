985a1ef6806026d7f0772435b8950fb0e2f692b2
jihyeonoh12 <jihyeonoh12@gmail.com>
2019-12-18 21:33:31 -0800

    third commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 var introBox = document.querySelector("#introBox");
 var startButton = document.querySelector("#button");
 var newQuestion = document.querySelector("#questionsHere");
-var firstChoice = document.querySelector("#firstA");
+var firstChoice = document.querySelector("#firstA")
 var secondChoice = document.querySelector("#secondA");
 var thirdChoice = document.querySelector("#thirdA");
 var fourthChoice = document.querySelector("#fourthA");
@@ -11,13 +11,23 @@ var listofAnswers = document.querySelector("#multipleChoice");
 var rightorWrong = document.querySelector("#rightorWrong");
 var scoreBox = document.querySelector("#viewScore");
 var shortAnswers = document.querySelector("#shortAnswer");
+var finishButton = document.querySelector("#finishBox");
+var submitButton = document.querySelector("#submitBox");
+var initialField = document.querySelector("#initialBox");
+var DoneButton = document.querySelector("#DoneBox");
 
 var scores = 0;
 var timer = 0;
 var questionIndex = 0;
 
-var userA ="";
+var userA = document.createElement("INPUT");
+var finishBtn = document.createElement("BUTTON");
+var submitBtn = document.createElement("BUTTON");
+
 var userInput = userA.value;
+var totalScore = 0;
+
+var  userName ="";
 
 
 var listofQuestions = [
@@ -51,41 +61,32 @@ var listofQuestions = [
 ];
 
 
-//first question
-function startQuiz() {
+
+function showQuestion() {
 
     introBox.remove();
     scoreBox.innerText = "Score " + scores;
 
-    newQuestion.innerText = listofQuestions[0].title;
-
-    // var btn1 =document.createElement("BUTTON"); 
-    // btn1.innerText = listofQuestions[0].choices[0];
-    // firstChoice.appendChild(btn1);
-
-    firstChoice.innerText = listofQuestions[0].choices[0];
-    secondChoice.innerText = listofQuestions[0].choices[1];
-    thirdChoice.innerText = listofQuestions[0].choices[2];
-    fourthChoice.innerText = listofQuestions[0].choices[3];
-
-    //right Answer
-    thirdChoice.addEventListener("click", click);
-
-    //wrong Answer
-    firstChoice.addEventListener("click", click);
-    secondChoice.addEventListener("click", click);
-    fourthChoice.addEventListener("click", click);
-
-
-}
-
-function showQuestion() {
     newQuestion.innerText = listofQuestions[questionIndex].title;
+
+    firstChoice.className = "optionBox" ;
+    secondChoice.className ="optionBox" ;
+    thirdChoice.className ="optionBox" ;
+    fourthChoice.className ="optionBox" ;
 
     firstChoice.innerText = listofQuestions[questionIndex].choices[0];
     secondChoice.innerText = listofQuestions[questionIndex].choices[1];
     thirdChoice.innerText = listofQuestions[questionIndex].choices[2];
     fourthChoice.innerText = listofQuestions[questionIndex].choices[3];
+
+        //right Answer
+        thirdChoice.addEventListener("click", click);
+
+        //wrong Answer
+        firstChoice.addEventListener("click", click);
+        secondChoice.addEventListener("click", click);
+        fourthChoice.addEventListener("click", click);
+    
 }
 
 function click(event) {
@@ -104,12 +105,7 @@ function click(event) {
 
     if (questionIndex == 4) {
 
-
-
-
         showLastQuestion();
-
-
     }
 
     showQuestion();
@@ -121,46 +117,49 @@ function showLastQuestion() {
 
     newQuestion.innerText = listofQuestions[4].title;
 
-    firstChoice.innerText = "";
-    secondChoice.innerText = "";
-    thirdChoice.innerText = "";
-    fourthChoice.innerText = "";
-    rightorWrong.innerText = "";
+    firstChoice.remove();
+    secondChoice.remove();
+    thirdChoice.remove();
+    fourthChoice.remove();
 
 
     var userA = document.createElement("INPUT");
     userA.setAttribute("type", "text");
-    userA.innerText = "write here";
-    document.body.appendChild(userA);
+    shortAnswers.appendChild(userA);
 
-
-    var userInput = userA.value;
 
     var submitBtn = document.createElement("BUTTON");
     submitBtn.innerText = "enter";
-    document.body.appendChild(submitBtn);
+    submitButton.appendChild(submitBtn);
 
     submitBtn.addEventListener("click", lastAnswer);
 
-
 }
+
+
 
 function lastAnswer() {
 
     
     var userInput = userA.value;
     
-
     if (listofQuestions[4].answer === userInput) {
         correctA();
+      
     } else {
+
         wrongA();
+        
     }
 
-    submitBtn.remove();
+   shortAnswers.remove();
+   submitButton.remove();
+
+    
     var finishBtn = document.createElement("BUTTON");
+    
     finishBtn.innerText = "finish";
-    document.body.appendChild(finishBtn);
+    finishButton.appendChild(finishBtn);
 
     finishBtn.addEventListener("click", lastPage);
 
@@ -168,51 +167,92 @@ function lastAnswer() {
 }
 
 
+
+function lastPage() {
+
+
+    finishButton.remove();
+
+    newQuestion.innerHTML = "Please insert your initial";
+
+    var userInitial = document.createElement("INPUT");
+    userInitial.setAttribute("type", "text");
+    initialField.appendChild(userInitial);
+
+
+    var DoneBtn = document.createElement("BUTTON");
+    DoneBtn.innerText = "Done";
+    DoneButton.appendChild(DoneBtn);
+    DoneBtn.addEventListener("click", thankYouPage);
+
+}
+
+
+
+function thankYouPage() {
+
+    DoneButton.remove();
+    initialField.remove();
+
+    userName = userInitial.innerText;
+
+    // Show Thank you + name
+    newQuestion.innerText = "Thank you!" + userName;
+
+
+    var restartBtn = document.createElement("DIV");
+    restartBtn.innerHTML = "<button>submit</button>";
+    btnContainer.appendChild(restartBtn);
+    restartBtn.addEventListener("click", storeScores);
+
+}
+
+
+
+
 function correctA() {
     rightorWrong.innerText = "correct!";
     scores++;
     scoreBox.innerText = "Score " + scores;
 
+    setInterval(delayText,2000);
+
 }
 
 function wrongA() {
     rightorWrong.innerText = "wrong!";
+    
+    setInterval(delayText,2000);
+
+}
+
+function delayText() {
+    rightorWrong.innerText = "";
 
 }
 
 
 function storeScores() {
 
+    
+    // totalScore = initialField.innerText + scores;
+    localStorage.setItem('totalScore', JSON.stringify(initialField.innerText + scores));
+
+}
+
+
+function viewScore() {
+
     var retrievedInfo = localStorage.getItem('totalScore');
     console.log('totalScore: ', JSON.parse(retrievedInfo));
 
-
-}
-
-function lastPage() {
-
-    
-    userA.remove();
-    finishBtn.remove();
-    rightorWrong.innerText = "";
-
-    newQuestion.innerText = "Please insert your initial";
-
-    var userInitial = document.createElement("INPUT");
-    userInitial.setAttribute("type", "text");
-    document.body.appendChild(userInitial);
-
-    var totalScore = userInitial.value + scores
-
-    localStorage.setItem('totalScore', JSON.stringify(testObject));
-
-
-
 }
 
 
 
 
 
-startButton.addEventListener("click", startQuiz);
+
+
+startButton.addEventListener("click", showQuestion);
 
